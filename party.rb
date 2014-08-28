4143e12e4f42f5af04f921f5cde02679125f977e
Jason Lovell <jason@zoomcc.com>
2014-08-27 23:58:13 -0500

    expand random theme list

--- party.rb
+++ party.rb
@@ -4,9 +4,24 @@ require 'haml'
 require 'sass'
 
 RANDOMS = [
-  'war of 1812',
-  'water-less pool',
-  'celebrity couples'
+  'a war of 1812 party',
+  'a water-less pool party',
+  'christmas in august',
+  'a celebrity couples party',
+  'an angels and devils party',
+  'a masquerade',
+  'a medieval party',
+  'a hawaiian luau',
+  'a casino party',
+  'a toga party',
+  'a mustache party',
+  'a wild west party',
+  'a ninja party',
+  'a neon party',
+  'a groovy disco',
+  'a pirate party',
+  'a crazy hat party',
+  'a frat/sorority party'
 ]
 
 get '/' do end
@@ -15,7 +30,7 @@ get '/favicon.ico' do end
 get '/:name' do |name|
   name &&= name.downcase
   cookie = request.cookies['invitee']
-  # return "cheater" if cookie && cookie != name
+  # return "cheater" if cookie && !name[cookie]
   response.set_cookie 'invitee', name
 
   @name =
@@ -27,8 +42,8 @@ get '/:name' do |name|
 
   @theme =
   case name
-  when 'jason' then 'mexican fiesta'
-  when 'will'  then 'spring break'
+  when 'jason' then 'a mexican fiesta'
+  when 'will'  then 'a spring break party'
   else
     RANDOMS[Random.new(name.codepoints.inject(:+)).rand(RANDOMS.size)]
   end
@@ -37,17 +52,19 @@ get '/:name' do |name|
 end
 
 post '/respond' do
-  Pony.mail to:      'jason@zoomcc.com',
-            subject: "Theme party response #{params[:response]}",
-            body:    "#{params[:name][1..-1]} - #{params[:response]}",
-            via:     :smtp,
-            via_options: {
-              address:              'smtp.gmail.com',
-              port:                 '587',
-              enable_starttls_auto: true,
-              user_name:            'jason@zoomcc.com',
-              password:             ENV['GMAIL_KEY'] || 'rvgxxkzrhlfnboev',
-              authentication:       :plain,
-              domain:               "party.thwovell.com"
-            }
+  Pony.mail({
+    to:      "jason@zoomcc.com",
+    subject: "Theme party response #{params[:response]}",
+    body:    "#{params[:name][1..-1]} - #{params[:response]}",
+    via:     :smtp,
+    via_options: {
+      address:              'smtp.gmail.com',
+      port:                 '587',
+      enable_starttls_auto: true,
+      user_name:            'jason@zoomcc.com',
+      password:             ENV['GMAIL_KEY'] || 'rvgxxkzrhlfnboev',
+      authentication:       :plain,
+      domain:               "party.thwovell.com"
+    }
+  })
 end
