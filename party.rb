36f629a8fedf214da0856dd596fe1295cceb79ba
Jason Lovell <jason@zoomcc.com>
2014-08-27 20:09:57 -0500

    add inactive cookie invitee check

--- party.rb
+++ party.rb
@@ -2,8 +2,15 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 
+get '/' do end
+get '/favicon.ico' do end
+
 get '/:name' do |name|
   name &&= name.downcase
+  cookie = request.cookies['invitee']
+  # return "cheater" if cookie && cookie != name
+  response.set_cookie 'invitee', name
+
   @name =
   case name
   when 'jason' then 'Jason Lovell'
@@ -17,35 +24,5 @@ get '/:name' do |name|
   else ['war of 1812', 'water-less pool', 'celebrity couples'].sample
   end
 
-  haml :index
+  haml :party
 end
-
-__END__
-
-@@index
-%p
-  = @name ? "#{@name}, you're" : "You're"
-  invited to a #{@theme} party on Friday.
-
-@@layout
-%html
-  %head
-    %title #{@name}'s Invitation
-    <link href='http://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
-  :scss
-    body {
-      font-family: 'Lato', Calibri, Arial, sans-serif;
-      color: #ffe8e7;
-      background: #E96D65;
-      font-weight: 300;
-      overflow-x: hidden;
-    }
-    .main {
-      max-width: 50em;
-      padding: 2em;
-      margin: 0 auto;
-    }
-    p { font-size: 4em; }
-  %body
-    .main
-      = yield
