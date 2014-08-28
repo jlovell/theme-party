8f94ff6cf398a80562c28cd6859f2190ac405890
Jason Lovell <jason@zoomcc.com>
2014-08-27 23:06:52 -0500

    add email confirmation and random seed for name

--- party.rb
+++ party.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
+require 'pony'
 require 'haml'
 require 'sass'
+
+RANDOMS = [
+  'war of 1812',
+  'water-less pool',
+  'celebrity couples'
+]
 
 get '/' do end
 get '/favicon.ico' do end
@@ -15,14 +22,32 @@ get '/:name' do |name|
   case name
   when 'jason' then 'Jason Lovell'
   when 'will'  then 'Will Thwaites'
+  else name.capitalize
   end
 
   @theme =
   case name
   when 'jason' then 'mexican fiesta'
   when 'will'  then 'spring break'
-  else ['war of 1812', 'water-less pool', 'celebrity couples'].sample
+  else
+    RANDOMS[Random.new(name.codepoints.inject(:+)).rand(RANDOMS.size)]
   end
 
   haml :invite
+end
+
+post '/respond' do
+  Pony.mail to:      'jason@zoomcc.com',
+            subject: "Theme party response #{params[:response]}",
+            body:    "#{params[:name][1..-1]} - #{params[:response]}",
+            via:     :smtp,
+            via_options: {
+              address:              'smtp.gmail.com',
+              port:                 '587',
+              enable_starttls_auto: true,
+              user_name:            'jason@zoomcc.com',
+              password:             ENV['GMAIL_KEY'] || 'rvgxxkzrhlfnboev',
+              authentication:       :plain,
+              domain:               "party.thwovell.com"
+            }
 end
